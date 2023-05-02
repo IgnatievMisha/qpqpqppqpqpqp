270fac50a11f206ba062938bb6c80463f43626e6
IgnatievMisha <mishaross1604@gmail.com>
2023-05-02 19:16:46 +0300

    fibonacci

--- main.py
+++ main.py
@@ -27,3 +27,20 @@ def delay_decorator(func):
 def sleepy():
     print("i`m sleep")
 sleepy()
+
+#________________________________________________
+
+
+def cache_decorator(func):
+    cache = {}
+    def wrapper(n):
+        if n not in cache:
+            cache[n] = func(n)
+        return cache[n]
+    return wrapper
+@cache_decorator
+def fibonacci(n):
+    if n in (0, 1):
+        return n
+    return fibonacci(n-1) + fibonacci(n-2)
+print(fibonacci(10))
