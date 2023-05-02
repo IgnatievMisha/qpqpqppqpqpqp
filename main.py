b3983aa194d5f6e3dddc12bfaa03a7faa8bd85d4
IgnatievMisha <mishaross1604@gmail.com>
2023-05-02 18:54:01 +0300

    zatrimka

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #1 decorators theory
+import time
 
 def my_decorator_func(func): #3
     def wrapper(): #4
@@ -12,3 +13,17 @@ def my_decorator_func(func): #3
 def say_hello(): #1
     print("Hello")
 say_hello()
+
+#____________________________________________________
+
+def delay_decorator(func):
+    def wrapper(*args, **kwargs):
+        time.sleep(3)
+        return func(*args, **kwargs)
+    return wrapper
+
+
+@delay_decorator
+def sleepy():
+    print("i`m sleep")
+sleepy()
